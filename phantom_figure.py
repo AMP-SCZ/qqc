cfb016ccce063aaa2341dcda95f13ff57eeae880
kcho <kevincho@bwh.harvard.edu>
2021-07-08 11:33:22 -0400

    feat: multiple number and type of inputs

--- phantom_figure.py
+++ phantom_figure.py
@@ -9,36 +9,72 @@ import nibabel as nb
 import numpy as np
 import matplotlib.pyplot as plt
 import tempfile
+import shutil
+from typing import List
+import math
 
 
 def convert_to_img(dicom_folder: str, outdir: str, name: str) -> None:
     '''Convert dicoms into nifti to a given location outdir/name'''
     command = f'dcm2niix \
-            -o {outdir} -f {name} {dicom_folder}'
-    subprocess.check_call(command, shell=True)
+            -o {outdir} -f {name} -z y -b y {dicom_folder}'
+    subprocess.run(command, shell=True, check=True, capture_output=True)
 
 
 def load_data_bval(prefix: Path):
     '''Return data array and bval array from the files with given prefix'''
-    data = nb.load(prefix.with_suffix('.nii')).get_fdata()
+    data = nb.load(prefix.with_suffix('.nii.gz')).get_fdata()
     bval = np.loadtxt(prefix.with_suffix('.bval'))
 
     return data, bval
 
 
-def get_data(dicom_folder: str, name: str, threshold: str):
+def get_diffusion_data_from_dicom_dir(dicom_dir: str,
+                            name: str,
+                            threshold: str,
+                            save_outputs: bool = False):
     '''Convert dicoms to load 4D dMRI data and threshold it before return'''
     temp_dir = tempfile.TemporaryDirectory()
-    convert_to_img(dicom_folder, temp_dir.name, name)
+    convert_to_img(dicom_dir, temp_dir.name, name)
     data, bval_arr = load_data_bval(Path(temp_dir.name) / name)
     data = data[:, :, :, bval_arr < threshold]
     bval_arr = bval_arr[bval_arr < threshold]
+
+    if save_outputs:
+        shutil.copytree(temp_dir.name, name)
+
     temp_dir.cleanup()
 
     return data, bval_arr
 
 
-def create_b0_signal_figure(data1: np.array, data1_bval: np.array,
+def get_diffusion_data_from_nifti_prefix(nifti_prefix: str,
+                            name: str,
+                            threshold: str,
+                            save_outputs: bool = False):
+    '''Convert dicoms to load 4D dMRI data and threshold it before return'''
+    data, bval_arr = load_data_bval(nifti_prefix)
+    data = data[:, :, :, bval_arr < threshold]
+    bval_arr = bval_arr[bval_arr < threshold]
+
+    return data, bval_arr
+
+
+def get_diffusion_data_from_nifti_dir(nifti_dir: str,
+                            name: str,
+                            threshold: str,
+                            save_outputs: bool = False):
+    '''Convert dicoms to load 4D dMRI data and threshold it before return'''
+    nifti_prefix = list(Path(nifti_dir).glob('*.nii.gz'))[0]
+    data, bval_arr = load_data_bval(
+            nifti_prefix.parent / nifti_prefix.name.split('.')[0])
+    data = data[:, :, :, bval_arr < threshold]
+    bval_arr = bval_arr[bval_arr < threshold]
+
+    return data, bval_arr
+
+
+def create_b0_signal_figure_prev(data1: np.array, data1_bval: np.array,
                             data2: np.array, data2_bval: np.array,
                             data3: np.array, data3_bval: np.array,
                             out: str, savefig: bool = False):
@@ -63,7 +99,7 @@ def create_b0_signal_figure(data1: np.array, data1_bval: np.array,
         ax.plot(data_mean, color+'-')
         ax.plot(data_mean, color+'o')
         ax.set_ylabel("Average signal in all voxels")
-        ax.set_xlabel("B0 value")
+        ax.set_xlabel("B value")
         ax.set_title(name)
         ax.set_xticks(np.arange(len(bval)))
         ax.set_xticklabels(bval)
@@ -88,31 +124,130 @@ def create_b0_signal_figure(data1: np.array, data1_bval: np.array,
         return fig
 
 
-def parse_args(argv):
+def create_b0_signal_figure(dataset: List[tuple], out: str,
+                            savefig: bool = False):
+    '''Plot b0 summary from three different 4d dMRI volumes
+
+    Key arguments:
+        data1: first AP B0 volumes
+        data2: second AP B0  volumes
+        data3: b0 volumes extracted from the PA dMRI
+        out: output image file name, eg) test.png
+        savefig: save figure if True
+    '''
+    col_num = 3
+    col_width = 4
+    row_num = math.ceil(len(dataset) / col_num)
+    row_height = 8
+    width = len(dataset) * col_width
+    height = row_num * row_height
+
+    fig, axes = plt.subplots(ncols=col_num, nrows=row_num,
+            figsize=(width, height), dpi=150)
+
+    # color
+    cm = plt.get_cmap('brg')
+
+    color_num = 0
+    for ax, (data, bval, name) in zip(np.ravel(axes), dataset):
+        color = cm(1.*color_num/len(dataset))
+        data_mean = [data[:, :, :, vol_num].mean() for vol_num in
+                np.arange(data.shape[-1])]
+        ax.plot(data_mean, color=color, linestyle='-', marker='o')
+        ax.set_ylabel("Average signal in all voxels")
+        ax.set_xlabel("B0 value")
+        ax.set_title(name)
+        ax.set_xticks(np.arange(len(bval)))
+        ax.set_xticklabels(bval)
+        color_num += 1
+
+    max_y = 0
+    min_y = 100000
+    for data in [x[0] for x in dataset]:
+        values = np.array(
+                [data[:, :, :, vol_num].mean() for vol_num
+                    in np.arange(data.shape[-1])])
+        max_y = values.max() if values.max() > max_y else max_y
+        min_y = values.min() if values.min() < min_y else min_y
+
+    for ax in np.ravel(axes):
+        ax.set_ylim(min_y-5, max_y+5)
+
+    for ax in np.ravel(axes)[len(dataset):]:
+        ax.axis('off')
+
+    fig.subplots_adjust(wspace=0.3)
+
+    if savefig:
+        fig.savefig(out)
+    else:
+        return fig
+
+
+def parse_args():
     '''Parse inputs coming from the terminal'''
     parser = argparse.ArgumentParser(description='Summarize phantom')
 
-    parser.add_argument('--apb0dir1', type=str, help='First AP b0 dicom dir')
-    parser.add_argument('--apb0dir2', type=str, help='Second AP b0 dicom dir')
-    parser.add_argument('--padmridir', type=str, help='PA dMRI dicom dir')
+    # mode
+    parser.add_argument('--mode', type=str, required=True,
+            choices=['dmri_b0', 'fmri', 'dmri', 'json_check'],
+            help='Select mode')
+
+    # image input related options
+    parser.add_argument('--dicom_dirs', type=str, nargs='+',
+            help='List of dicom directories to plot summary. They will be '
+                 'converted to nifti using dcm2niix to new directories.')
+    parser.add_argument('--nifti_prefixes', type=str, nargs='+',
+            help='List of nifti image prefixes to plot summary from.')
+    parser.add_argument('--nifti_dirs', type=str, nargs='+',
+            help='List of directories that contain unique nifti image')
+
+    # extra options
+    parser.add_argument('--names', type=str, nargs='+',
+            help='List of name for each given dicom dirs, nifti prefixes or '
+                 'nifti dirs')
+    parser.add_argument('--store_nifti', action='store_true',
+            help='Keep outputs of dcm2niix for later use.')
     parser.add_argument('--b0thr', type=int, default=50,
             help='b0 threshold, default=50')
-    parser.add_argument('--out', type=str, help='Out image file')
-    args = parser.parse_args(argv)
+    parser.add_argument('--out_image', type=str, help='Out image file')
+
+
+    args = parser.parse_args()
     return args
 
 
+def dmri_b0_summary(args):
+    if args.mode == 'dmri_b0':
+        if args.dicom_dirs:
+            function = get_diffusion_data_from_dicom_dir
+            vars = args.dicom_dirs
+        elif args.nifti_prefixes:
+            function = get_diffusion_data_from_nifti_prefix
+            vars = args.nifti_prefixes
+        elif args.nifti_dirs:
+            function = get_diffusion_data_from_nifti_dir
+            vars = args.nifti_dirs
+        else:
+            sys.exit('Data input needs to be provided. Provide one of '
+            '--dicom_dirs, --nifti_prefixes or --nifti_dirs. Exiting.')
+
+        dataset = []
+        for num, var in enumerate(vars):
+            name = args.names[num] if args.names else Path(var).name
+            data, bval_arr = function(var, name, args.b0thr, args.store_nifti)
+            dataset.append((data, bval_arr, name))
+
+        create_b0_signal_figure(dataset, args.out_image, True)
+
+
+def json_check(args):
+    if args.mode == 'json_check':
+        pass
+
 if __name__ == '__main__':
-    args = parse_args(sys.argv[1:])
+    args = parse_args()
 
-    ap_b0_1_data, ap_b0_1_bval_arr = get_data(
-            args.apb0dir1, 'apb0_1', args.b0thr)
-    ap_b0_2_data, ap_b0_2_bval_arr = get_data(
-            args.apb0dir2, 'apb0_2', args.b0thr)
-    pa_dmri_data, pa_dmri_bval_arr = get_data(
-            args.padmridir, 'padmri', args.b0thr)
-
-    create_b0_signal_figure(ap_b0_1_data, ap_b0_1_bval_arr,
-                            ap_b0_2_data, ap_b0_2_bval_arr,
-                            pa_dmri_data, pa_dmri_bval_arr,
-                            args.out, True)
+    # dmri_b0_summary
+    dmri_b0_summary(args)
+    json_check(args)
--- test.sh
+++ test.sh
@@ -1,5 +1,57 @@
-./phantom_figure.py  \
-  --apb0dir1 /data/predict/phantom_data/ProNET_UCLA/dMRI_b0_AP_20 \
-  --apb0dir2 /data/predict/phantom_data/ProNET_UCLA/dMRI_b0_AP_24 \
-  --padmridir /data/predict/phantom_data/ProNET_UCLA/dMRI_dir176_PA_22 \
-  --out test.png
+#/data/predict/phantom_data/phantom_check/phantom_figure.py \
+  #--apb0dir1 /data/predict/phantom_data/ProNET_UCLA/dMRI_b0_AP_20 \
+  #--apb0dir2 /data/predict/phantom_data/ProNET_UCLA/dMRI_b0_AP_24 \
+  #--padmridir /data/predict/phantom_data/ProNET_UCLA/dMRI_dir176_PA_22 \
+  #--out test.png
+
+#/data/predict/phantom_data/phantom_check/phantom_figure.py \
+    #--mode dmri_b0 \
+    #--dicom_dirs \
+        #/data/predict/phantom_data/ProNET_UCLA/dMRI_b0_AP_20 \
+        #/data/predict/phantom_data/ProNET_UCLA/dMRI_dir176_PA_22 \
+        #/data/predict/phantom_data/ProNET_UCLA/dMRI_b0_AP_24 \
+    #--names \
+        #apb0_1 pa_dmri apb0_2 \
+    #--out_image new_test.png
+
+
+
+#/data/predict/phantom_data/phantom_check/phantom_figure.py \
+    #--mode dmri_b0 \
+    #--dicom_dirs \
+        #/data/predict/phantom_data/ProNET_UCLA/dMRI_b0_AP_20 \
+        #/data/predict/phantom_data/ProNET_UCLA/dMRI_dir176_PA_22 \
+        #/data/predict/phantom_data/ProNET_UCLA/dMRI_b0_AP_24 \
+        #/data/predict/phantom_data/ProNET_UCLA/dMRI_b0_AP_20 \
+    #--names \
+        #apb0_1 pa_dmri apb0_2 dup \
+    #--out_image new_test_dup.png
+
+
+#/data/predict/phantom_data/phantom_check/phantom_figure.py \
+    #--mode dmri_b0 \
+    #--dicom_dirs \
+        #/data/predict/phantom_data/ProNET_UCLA/dMRI_b0_AP_20 \
+        #/data/predict/phantom_data/ProNET_UCLA/dMRI_dir176_PA_22 \
+        #/data/predict/phantom_data/ProNET_UCLA/dMRI_b0_AP_24 \
+        #/data/predict/phantom_data/ProNET_UCLA/dMRI_b0_AP_20 \
+    #--names \
+        #apb0_1 pa_dmri apb0_2 dup \
+    #--store_nifti \
+    #--out_image new_test_dup.png
+
+
+#/data/predict/phantom_data/phantom_check/phantom_figure.py \
+    #--mode dmri_b0 \
+    #--nifti_dirs apb0_1 pa_dmri apb0_2 \
+    #--names apb0_1 pa_dmri apb0_2 \
+    #--out_image new_test_nifti_dir.png
+
+
+
+/data/predict/phantom_data/phantom_check/phantom_figure.py \
+    --mode dmri_b0 \
+    --nifti_dirs apb0_1 pa_dmri apb0_2 \
+    --names apb0_1 pa_dmri apb0_2 \
+    --b0thr 5000 \
+    --out_image new_test_nifti_dir_thr5000.png
