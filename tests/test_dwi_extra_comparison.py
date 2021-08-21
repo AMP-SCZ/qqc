e9cd286f89f7bb930d4e27cb9d139b2627116dee
Kang Ik Kevin Cho <kevincho@bwh.harvard.edu>
2021-08-21 13:58:09 +0900

    feat: dwi bval comparison example added

--- tests/test_dwi_extra_comparison.py
+++ tests/test_dwi_extra_comparison.py
@@ -15,7 +15,7 @@ from phantom_check import compare_bval_files
 def test_parse_args():
     args = parse_args([
         '--bval_files',
-        '/data/predict/phantom_data/human_test/human_subject_0726/dMRI_dir176_PA_22/dMRI_dir176_PA_22.bval',
-        '/data/predict/phantom_data/human_test/human_subject_0729/test/22_dMRI_dir176_PA/22_dMRI_dir176_PA.bval'])
+        'first.bval',
+        'second.bval'])
 
     compare_bval_files(args.bval_files)
