8181a690b0f92c194fa85193fd7328fa1bfdb830
Kang Ik Kevin Cho <kevincho@bwh.harvard.edu>
2021-09-03 17:03:30 -0400

    test: init

---
+++ tests/scripts/test_extract_given_field_from_dicom.py
@@ -0,0 +1,29 @@
+import phantom_check
+import os
+from pathlib import Path
+
+import sys
+lochness_root = Path(phantom_check.__path__[0]).parent
+scripts_dir = lochness_root / 'scripts'
+sys.path.append(str(scripts_dir))
+
+from extract_given_field_from_dicom import write_dicom_information_as_text
+
+
+def read_and_delete(file_loc):
+    with open(file_loc, 'r') as f:
+        print(f.read())
+
+    os.remove(file_loc)
+
+
+def test_simple():
+    write_dicom_information_as_text('example.dcm', '0029', '1020')
+
+
+def test_with_output_file():
+    write_dicom_information_as_text('example.dcm', '0029',
+                                    '1020', 'test_out.txt')
+    read_and_delete('test_out.txt')
+
+
