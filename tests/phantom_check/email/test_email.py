7e129ba00012a1ef66af185b4e9d78663e2bc2ec
kc244 <kevincho@bwh.harvard.edu>
2022-02-03 14:00:06 -0500

    feat: email html

--- tests/phantom_check/email/test_email.py
+++ tests/phantom_check/email/test_email.py
@@ -1,8 +1,22 @@
 from phantom_check.email import send_out_qqc_results, send
 from pathlib import Path
 import pandas as pd
+import os
 pd.set_option('max_rows', 5000)
 
+
+def test_format():
+    from jinja2 import Environment, FileSystemLoader
+    import phantom_check
+    email_template_dir = os.path.join(Path(phantom_check.email.__dir__))
+    print(email_template_dir)
+
+    env = Environment(loader=FileSystemLoader(str(email_template_dir)))
+    template = env.get_template('bootdey_template.html')
+    html_str = template.render()
+    
+    with open('test.html', 'w') as f:
+        f.write(html_str)
 
 def test_send_out_qqc_results():
     root_dir = Path('/data/predict/kcho/flow_test/MRI_ROOT')
