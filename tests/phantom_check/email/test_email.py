17cdeb2b5d896be601dd92c08b0c101331bccc28
kc244 <kevincho@bwh.harvard.edu>
2022-02-03 14:01:56 -0500

    feat: email html

--- tests/phantom_check/email/test_email.py
+++ tests/phantom_check/email/test_email.py
@@ -10,14 +10,24 @@ def test_format():
     import phantom_check
     email_template_dir = os.path.join(Path(phantom_check.email.__dir__))
     print(email_template_dir)
+    from email.mime.text import MIMEText
 
     env = Environment(loader=FileSystemLoader(str(email_template_dir)))
     template = env.get_template('bootdey_template.html')
     html_str = template.render()
+    import smtplib
     
     with open('test.html', 'w') as f:
         f.write(html_str)
 
+    msg = MIMEText(html_str, 'html')
+    msg['Subject'] = 'test_title'
+    msg['From'] = 'kevincho@bwh.harvard.edu'
+    msg['To'] = 'kevincho@bwh.harvard.edu'
+
+    s = smtplib.SMTP('localhost')
+    s.sendmail('kevincho@bwh.harvard.edu', 'kevincho@bwh.harvard.edu', msg.as_string())
+    
 def test_send_out_qqc_results():
     root_dir = Path('/data/predict/kcho/flow_test/MRI_ROOT')
     qqc_out_dir = root_dir / 'derivatives/quick_qc/sub-NL00000/ses-202112071'
