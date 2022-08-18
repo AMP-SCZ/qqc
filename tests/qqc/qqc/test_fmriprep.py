223cb2ea5286febbc4ecfc53a5114460495f5a04
kcho <kevincho@bwh.harvard.edu>
2022-08-18 17:30:18 -0400

    feat: fmriprep function test added

---
+++ tests/qqc/qqc/test_fmriprep.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+from qqc.qqc.fmriprep import run_fmriprep_on_data
+
+
+def test_run_preproc():
+    root_dir = Path('/data/predict/kcho/flow_test/MRI_ROOT')
+    rawdata_dir = root_dir / 'rawdata'
+    subject_id = 'sub-NL00000'
+    session_id = 'ses-202112071'
+    fmriprep_outdir_root = root_dir / 'derivatives/fmriprep'
+    fs_outdir_root = root_dir / 'derivatives/freesurfer'
+
+    run_fmriprep_on_data(
+            rawdata_dir,
+            subject_id,
+            session_id,
+            fmriprep_outdir_root,
+            fs_outdir_root=fs_outdir_root)
+
+
+
+def test_run_preproc_real_participant():
+    root_dir = Path('/data/predict/data_from_nda/MRI_ROOT')
+    rawdata_dir = root_dir / 'rawdata'
+    subject_id = 'sub-OR00697'
+    session_id = 'ses-202208151'
+    fmriprep_outdir_root = root_dir / 'derivatives/fmriprep'
+    fs_outdir_root = root_dir / 'derivatives/freesurfer'
+
+    run_fmriprep_on_data(
+        rawdata_dir,
+        subject_id,
+        session_id,
+        fmriprep_outdir_root,
+        fs_outdir_root=fs_outdir_root)
+
