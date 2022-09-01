29a20b09556de6ce7c671a0f47877d40f9384c78
kc244 <kevincho@bwh.harvard.edu>
2022-09-01 15:51:43 -0400

    feat: replace tabs with four spaces

--- lib/asana_api.py
+++ lib/asana_api.py
@@ -11,22 +11,22 @@ def read_token() -> str:
 
 
 def create_new_task(potential_subject: str):
-	#Creates new task to send to AMP SCZ project in Asana
-	new_task = {'name': potential_subject, 
-		'note': 'New Data has been uploaded for ' + potential_subject,
-		'assignee': 'kevincho@bwh.harvard.edu',
-		'projects': [amp_scz_gid]}
+    #Creates new task to send to AMP SCZ project in Asana
+    new_task = {'name': potential_subject, 
+        'note': 'New Data has been uploaded for ' + potential_subject,
+        'assignee': 'kevincho@bwh.harvard.edu',
+        'projects': [amp_scz_gid]}
 
-	created_task = client.tasks.create_in_workspace('958915379528887', new_task)
-	
-	return created_task
+    created_task = client.tasks.create_in_workspace('958915379528887', new_task)
+    
+    return created_task
 
 
 def update_task_list(): 
-	tasks = client.tasks.get_tasks_for_project(amp_scz_gid)
-	tasks_list = list(tasks)
-	print('Returning updated list of all tasks in AMP SCZ project')
-	return tasks_list
+    tasks = client.tasks.get_tasks_for_project(amp_scz_gid)
+    tasks_list = list(tasks)
+    print('Returning updated list of all tasks in AMP SCZ project')
+    return tasks_list
 
 
 
@@ -34,11 +34,11 @@ def update_task_list():
 
 
 '''def create_new_subtask():
-	new_subtask = {'name': 'MRI',
-	'note': 'MRI data for subject_id has arrived, please do QC!',
-	'assignee': 'ojohn@bwh.harvard.edu', #person responsible for doing MRI QC 
-	'projects': [amp_scz_gid],
-	'dependents': [update_task_list()[0]['gid']]}'''
+    new_subtask = {'name': 'MRI',
+    'note': 'MRI data for subject_id has arrived, please do QC!',
+    'assignee': 'ojohn@bwh.harvard.edu', #person responsible for doing MRI QC 
+    'projects': [amp_scz_gid],
+    'dependents': [update_task_list()[0]['gid']]}'''
 '''hahah'''
 
 token = read_token()
