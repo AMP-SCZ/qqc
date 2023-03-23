69d0465e1c0532a3d090614f66857af55cdde36d
owenborders <111159438+owenborders@users.noreply.github.com>
2023-03-23 09:40:46 -0400

    Added new columns
    added the new columns, changed predict to predict1, added safeguard in zip date function, reorganized columns, removed brackets from other files column

--- ampscz_asana/lib/qc.py
+++ ampscz_asana/lib/qc.py
@@ -9,8 +9,8 @@ import os
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 
-phoenix_dir = Path('/data/predict/data_from_nda/Pronet/PHOENIX')
-phoenix_dir = Path('/data/predict/data_from_nda/Prescient/PHOENIX')
+phoenix_dir = Path('/data/predict1/data_from_nda/Pronet/PHOENIX')
+phoenix_dir = Path('/data/predict1/data_from_nda/Prescient/PHOENIX')
 
 
 def get_entry_date_from_run_sheet(run_sheet: Path) -> str:
@@ -54,7 +54,7 @@ def check_mri_data(run_sheet: Path, entry_date: str) -> bool:
 
 def check_when_transferred(subject: str, entry_date: str) -> bool:
     ''''return if there is MR data and entry date'''
-    mri_root = Path('/data/predict/data_from_nda/MRI_ROOT')
+    mri_root = Path('/data/predict1/data_from_nda/MRI_ROOT')
     source_root = mri_root / 'sourcedata'
     date_numbers_only = re.sub('[-_]', '', entry_date)
     subject_dir = source_root / subject
@@ -68,7 +68,7 @@ def check_when_transferred(subject: str, entry_date: str) -> bool:
 
 
 def is_qqc_executed(subject, entry_date) -> bool:
-    mri_root = Path('/data/predict/data_from_nda/MRI_ROOT')
+    mri_root = Path('/data/predict1/data_from_nda/MRI_ROOT')
     source_root = mri_root / 'sourcedata'
 
     date_numbers_only = re.sub('[-_]', '', entry_date)
@@ -84,6 +84,7 @@ def is_qqc_executed(subject, entry_date) -> bool:
     
     
 def date_of_zip(subject, entry_date, phoenix_dir):
+    formatted_entry_date = entry_date.replace("-", "_")
     if 'Pronet' in phoenix_dir:
         prefix = 'Pronet'
     else:
@@ -94,7 +95,7 @@ def date_of_zip(subject, entry_date, phoenix_dir):
         if dir_name.startswith(f'{prefix}{subject[:2]}'):
             pronet_dir = dir_name
             break
-    zip_file = Path(base_dir, pronet_dir, 'raw', subject, 'mri', f'{subject}_MR_{entry_date}_1.zip')
+    zip_file = Path(base_dir, pronet_dir, 'raw', subject, 'mri', f'{subject}_MR_{formatted_entry_date}_1.zip')
     if zip_file.exists():
         stat = zip_file.stat()
         timestamp = stat.st_mtime
@@ -104,7 +105,7 @@ def date_of_zip(subject, entry_date, phoenix_dir):
         return ''
 
 def date_of_qqc(subject, entry_date) -> str:
-    mri_root = Path('/data/predict/data_from_nda/MRI_ROOT')
+    mri_root = Path('/data/predict1/data_from_nda/MRI_ROOT')
     source_root = mri_root / 'sourcedata'
     date_numbers_only = re.sub('[-_]', '', entry_date)
     subject_dir = source_root / subject
@@ -120,7 +121,7 @@ def date_of_qqc(subject, entry_date) -> str:
 
 
 def is_mri_done(subject, entry_date) -> bool:
-    mri_root = Path('/data/predict/data_from_nda/MRI_ROOT')
+    mri_root = Path('/data/predict1/data_from_nda/MRI_ROOT')
     deriv_root = mri_root / 'derivatives' / 'mriqc'
 
     date_numbers_only = re.sub('[-_]', '', entry_date)
@@ -135,7 +136,7 @@ def is_mri_done(subject, entry_date) -> bool:
 
 
 def is_fmriprep_done(subject, entry_date) -> bool:
-    mri_root = Path('/data/predict/data_from_nda/MRI_ROOT')
+    mri_root = Path('/data/predict1/data_from_nda/MRI_ROOT')
     deriv_root = mri_root / 'derivatives' / 'fmriprep'
 
     date_numbers_only = re.sub('[-_]', '', entry_date)
@@ -150,7 +151,7 @@ def is_fmriprep_done(subject, entry_date) -> bool:
 
 
 def is_dwipreproc_done(subject, entry_date) -> bool:
-    mri_root = Path('/data/predict/data_from_nda/MRI_ROOT')
+    mri_root = Path('/data/predict1/data_from_nda/MRI_ROOT')
     deriv_root = mri_root / 'derivatives' / 'dwipreproc'
 
     date_numbers_only = re.sub('[-_]', '', entry_date)
@@ -163,6 +164,127 @@ def is_dwipreproc_done(subject, entry_date) -> bool:
     else:
         return False
 
+    
+
+def extract_variable_information(row, col, variable_name, excluded_values,value_list):
+    domain_type_dict = {'1': 'clinical measures', '2': 'EEG', '3': 'Neuroimaging', '4': 'cognition', '5': 'genetic and fluid biomarkers', '6': 'digital biomarkers', '7': 'speech sampling'}
+    reason_dict = {'M1' : 'Refusal - no reason provided',
+ 'M2' : 'Refusal - reason: had an unpleasant experience last time',
+ 'M3' : 'Refusal - reason: anxiety associated with the assessment domain',
+ 'M4' : 'Refusal - reason: too much time commitment - AMP SCZ assessments',
+ 'M5' : 'Refusal - reason: too much time commitment - other studies',
+ 'M6' : 'No show',
+ 'M7' : 'Not booked',
+ 'M8' : 'Not applicable',
+ 'M9' : 'Uncontrollable circumstance',
+ 'M10':  'Other reason'}
+
+    value = row[col]
+    date = row['chrmri_entry_date']
+
+    if col == variable_name and row[col] not in excluded_values:
+        if variable_name == 'missing_data_complete':
+            if row[col]=='2':
+                value = 'Complete'
+            elif row[col] == '0':
+                value = 'Incomplete'
+        elif variable_name == 'chrmiss_domain':
+            if row[col] == '1':
+                value = 'Entire domain selected as missing'
+        elif 'chrmiss_domain_type' in variable_name:
+            #print(variable_name)
+            for key, item in domain_type_dict.items():
+                if key in col and row[col] == '1':
+                    value = item
+        elif 'chrmiss_domain_spec' in variable_name:
+            for key, item in reason_dict.items():
+                if key in row[col]:
+                    value = item
+        value_list.append(f"Timepoint: {row['redcap_event_name']} | Date: {date} | {value}")
+    elif (variable_name == 'comment') and (('mri' in col and 'comment' in col) and row[col] != '')  or (col == 'chrmri_missing'and row[col] != '') or (col == 'chrmri_missing_spec'and row[col] != ''):
+        if row[col] in row.values():
+            if ('mri' in col and 'comment' in col and row[col] != ''):
+                value_list.append(f"Timepoint: {row['redcap_event_name']} | Date: {date} | {row[col]}")
+    return value_list
+
+
+
+
+
+def extract_missing_data_information(subject, directory):
+    if 'Pronet' in directory:
+        directory = 'Pronet'
+    else:
+        directory = 'Prescient'
+    json_path = Path(f'/data/predict1/data_from_nda/{directory}/PHOENIX/PROTECTED/{directory}{subject[:2]}/raw/{subject}/surveys/{subject}.{directory}.json')
+    if json_path.exists():
+        with open(json_path, 'r') as f:
+            json_data = json.load(f)
+    else:
+        print(json_path)
+        return ['Json not found','Json not found','Json not found','Json not found','Json not found' ]
+    comments_list = []
+    missing_data_form_complete_list = []  #missing_data_complete
+    domain_missing_list = []   #chrmiss_domain
+    reason_for_missing_data_list = []   #chrmiss_domain_spec
+    domain_type_missing_list = [] #chrmiss_domain_type
+    variables = []
+    for x in range(1,8):
+        variables.append('chrmiss_domain_type' + f'___{x}')
+    variable_dict = {'variables':[{'missing_data_complete':['']},{'chrmiss_domain':['']},{'chrmiss_domain_spec':['']}]}
+    for x in range(0,len(variables)):
+        variable_dict['variables'].append({variables[x]:['','0']})
+    value_lists = [missing_data_form_complete_list,domain_missing_list,reason_for_missing_data_list]
+    for row in json_data:
+        print(row['chric_record_id'])
+        for col in row:
+            comments_list = extract_variable_information(row,col,'comment','',comments_list)
+            for x in range(0,len(variable_dict['variables'])):
+                for key, item in variable_dict['variables'][x].items():
+                    if 'miss_domain_type' not in key:
+                        value_lists[x] = extract_variable_information(row,col,key,item,value_lists[x])
+                    else:
+                        domain_type_missing_list = extract_variable_information(row,col,key,item,domain_type_missing_list)
+    list_of_lists = [domain_type_missing_list, reason_for_missing_data_list, domain_missing_list,missing_data_form_complete_list, comments_list]
+    for x in range(0,len(list_of_lists)):
+        list_of_lists[x] = list(set(list_of_lists[x]))
+        list_of_lists[x] = str(list_of_lists[x]).replace(']','').replace('[','').replace('\r\n','').replace('N\\A','').replace('\n','')
+        list_of_lists[x] = re.sub(r'\s+', ' ', list_of_lists[x])
+
+    return list_of_lists
+
+
+
+def compare_dates(df):
+    df['entry_date'] = df['entry_date'].str.replace('_', '-')
+    for index, row in df.iterrows():
+        entry_date = row['entry_date']
+        print(entry_date)
+        if pd.isna(entry_date):
+            df.drop(index, inplace=True)
+            continue
+        for col in ['domain_type_missing', 'reason_for_missing_data', 'domain_missing', 'missing_data_form_complete', 'comments']:
+            string_list = row[col].split(',')
+            for i in range(len(string_list)):
+                string_list[i] = string_list[i].replace('"', '').replace("'", '')
+                if string_list[i].count('|') >= 2:
+                    date_str = string_list[i].split('|')[1].strip()[6:]
+                    date_str = date_str.replace('_', '-')
+                    if date_str != entry_date or date_str == '':
+                        string_list[i] = ''
+            string_list = [s for s in string_list if s != '']
+            row[col] = ','.join(string_list)
+        df.loc[index] = row
+    return df
+
+
+    
+    
+    
+    
+    
+    
+    
 def get_run_sheet_df(phoenix_dir: Path, datatype='mri') -> pd.DataFrame:
     run_sheets = grep_run_sheets(phoenix_dir)
 
@@ -171,6 +293,7 @@ def get_run_sheet_df(phoenix_dir: Path, datatype='mri') -> pd.DataFrame:
     df['subject'] = df.file_loc.str.extract('([A-Z]{2}\d{5})')
     df['datatype'] = df.file_path.apply(lambda x: x.name.split('_')[2])
     df['other_files'] = df['file_loc'].apply(lambda x: os.listdir(Path(x).parent.absolute()))
+    df['other_files'] = df['other_files'].apply(lambda x: ', '.join(x))     
 
     
 
@@ -193,6 +316,16 @@ def get_run_sheet_df(phoenix_dir: Path, datatype='mri') -> pd.DataFrame:
     datatype_df.loc[index_with_mri_data, 'mri_arrival_date'] = \
             datatype_df.loc[index_with_mri_data].apply(lambda x:
                 check_when_transferred(x['subject'], x['entry_date']), axis=1)
+    missing_data_info = datatype_df.apply(lambda x: pd.Series(extract_missing_data_information(x['subject'], str(phoenix_dir))), axis=1)
+    datatype_df[['domain_type_missing', 'reason_for_missing_data', 'domain_missing', 'missing_data_form_complete', 'comments']] = missing_data_info.iloc[:, [0, 1, 2, 3, 4]]
+    cols_to_split = ['domain_type_missing', 'reason_for_missing_data', 'domain_missing', 'missing_data_form_complete', 'comments']
+    for x in range(0,len(cols_to_split)):
+        mask = datatype_df[cols_to_split[x]].notnull() & (datatype_df[cols_to_split[x]] != '')
+        datatype_df.loc[mask, 'timepoint'] = datatype_df[mask][cols_to_split[x]].str.split('|').str[0].str.split(': ').str[1]
+
+    for col in cols_to_split:
+        datatype_df[col] = datatype_df[col].str.replace(r'^.*\|([^|]*$)', r'\1', regex=True)
+
 
     datatype_df['qqc_executed'] = datatype_df.apply(lambda x:
             is_qqc_executed(x['subject'], x['entry_date']), axis=1)
@@ -211,7 +344,10 @@ def get_run_sheet_df(phoenix_dir: Path, datatype='mri') -> pd.DataFrame:
  
     datatype_df['zip_date'] = datatype_df.apply(lambda x, param:
             date_of_zip(x['subject'], x['entry_date'], param), axis=1, param=str(phoenix_dir))
-    
+    cols = datatype_df.columns.tolist()
+
+    new_cols = ['subject', 'entry_date','timepoint'] + [col for col in cols if col not in ['subject', 'entry_date', 'timepoint', 'file_loc']]
+    datatype_df = datatype_df[new_cols]
     return datatype_df
 
     # for _, i in datatype_df[~datatype_df.check_data].iterrows():
