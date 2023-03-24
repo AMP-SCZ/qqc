cd2e285817559e1902974be45deba697f47c228c
owenborders <111159438+owenborders@users.noreply.github.com>
2023-03-23 21:42:10 -0400

    Added test_compare_dates function

--- tests/ampscz_asana/lib/test_qc.py
+++ tests/ampscz_asana/lib/test_qc.py
@@ -13,12 +13,7 @@ def test_date_of_zip():
   assert(date_of_zip('ME04934', '2022_12_02', dir)) == '2023-03-13'
   assert(date_of_zip('CP01128', '2023_02_23', dir)) == '2023-03-02'
          
-         
-
     
-
-
-
 def test_extract_missing_data_information():  #will also test the extract_variable_information function since this one uses it 
   dir = '/data/predict1/data_from_nda/Prescient/PHOENIX'
   try:
@@ -38,4 +33,33 @@ def test_extract_missing_data_information():  #will also test the extract_variab
   
   
   
-#def test_compare_dates():
+import pandas as pd
+
+def test_compare_dates():
+    data = {
+        'entry_date': ['2022_10_14', '2022_10_15', '2022-11-15'],
+        'domain_type_missing': ['"Timepoint: baseline_arm_1 | Date: 2022-10-14 | clinical measures"',
+                                '"Timepoint: month_3_arm_1 | Date: | digital biomarkers"',
+                                '"Timepoint: month_6_arm_1 | Date: 2022-11-15 | clinical measures"'],
+        'reason_for_missing_data': ['','',''],
+        'domain_missing' : ['','',''],
+        'comments':['','',''],
+        'missing_data_form_complete':['','','']
+    }
+    df = pd.DataFrame(data)
+
+    output_df = compare_dates(df)
+
+    try:
+        assert set(output_df.columns) == set(data.keys()), "incorrect columns"
+        assert output_df.shape[0] == len(data['entry_date']), "incorrect number of rows"
+        assert output_df['domain_type_missing'].tolist() == ['Timepoint: baseline_arm_1 | Date: 2022-10-14 | clinical measures', '', 'Timepoint: month_6_arm_1 | Date: 2022-11-15 | clinical measures'], "incorrect values in 'domain_type_missing' column"
+        assert output_df['reason_for_missing_data'].tolist() == ['', '', ''], "incorrect values in 'reason_for_missing_data' column"
+        assert output_df['domain_missing'].tolist() == ['', '', ''], "incorrect values in 'domain_missing' column"
+        assert output_df['comments'].tolist() == ['', '', ''], "incorrect values in 'comments' column"
+        assert output_df['missing_data_form_complete'].tolist() == ['', '', ''], "incorrect values in 'missing_data_form_complete' column"
+    except AssertionError:
+         raise AssertionError(f"Assertion error: {AssertionError}")
+    
+    print("All tests passed")
+    
