1e462b5faa2e2ca2e82b488f264d1f6b5afa85fb
owenborders <111159438+owenborders@users.noreply.github.com>
2023-03-27 13:21:39 -0400

    index fix #2

--- ampscz_asana/lib/qc.py
+++ ampscz_asana/lib/qc.py
@@ -462,6 +462,6 @@ def get_run_sheet_df(phoenix_dir: Path, datatype='mri') -> pd.DataFrame:
     datatype_df['Time between scan and data arrival'] = datatype_df.apply(
             arrival_scan_time, axis=1).apply(format_days)
     datatype_df.reset_index(drop=True,inplace=True)
-    datatype_df['index'] = ''
+    del datatype_df['index']
 
     return datatype_df
