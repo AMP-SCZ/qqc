c84b4b595ed390274d436a1cefe8240e689f1ebd
kcho <kevincho@bwh.harvard.edu>
2023-04-07 16:19:51 -0400

    init: mri_count test initiated

--- tests/ampscz_asana/lib/test_mri_count.py
+++ tests/ampscz_asana/lib/test_mri_count.py
@@ -1,77 +1,61 @@
 from ampscz_asana.lib.server_scanner import get_all_mri_zip, \
-        get_most_recent_file
+        get_most_recent_file, get_all_subjects_with_consent
+from ampscz_asana.lib.mri_count import get_mri_zip_df, \
+        get_mri_zip_df_pivot_for_subject, create_dpdash_mri_zip_df_pivot, \
+        add_qc_measures, get_mriqc_value_df_pivot_for_subject, \
+        create_dpdash_eeg_zip_df_pivot, create_dpdash_zip_df_pivot
 import pandas as pd
 from pathlib import Path
+import logging
+formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
+logging.basicConfig(format=formatter, handlers=[logging.StreamHandler()])
+logging.getLogger().setLevel(logging.DEBUG)
+
+pd.set_option('display.max_columns', 50)
 
 
-def test_load():
-    phoenix_root = Path('/data/predict1/data_from_nda/Pronet/PHOENIX')
-    zip_files = get_all_mri_zip(phoenix_root, test=True)
-    print(zip_files)
-    pass
+def test_get_mri_zip_df():
+    data_root = Path('/data/predict1/data_from_nda')
+    phoenix_paths = [data_root / 'Pronet/PHOENIX',
+                     data_root / 'Prescient/PHOENIX']
+    mriflow_csv = data_root / 'MRI_ROOT/flow_check/mri_data_flow.csv'
+    dpdash_outpath = data_root / 'MRI_ROOT/eeg_mri_count'
+
+    eeg_zip_df = get_mri_zip_df(phoenix_paths, mriflow_csv)
+    eeg_zip_df_pivot = get_mri_zip_df_pivot_for_subject(eeg_zip_df,
+                                                        phoenix_paths)
+    create_dpdash_zip_df_pivot(eeg_zip_df_pivot,
+                               dpdash_outpath)
 
 
-def test_mri_count():
-    mriqc_dir = Path('/data/predict1/data_from_nda/MRI_ROOT/derivatives/google_qc')
+def test_get_eeg_zip_df():
+    data_root = Path('/data/predict1/data_from_nda')
+    phoenix_paths = [data_root / 'Pronet/PHOENIX',
+                     data_root / 'Prescient/PHOENIX']
+    mriflow_csv = data_root / 'MRI_ROOT/flow_check/mri_data_flow.csv'
+    dpdash_outpath = data_root / 'MRI_ROOT/eeg_mri_count'
+
+    eeg_zip_df = get_mri_zip_df(phoenix_paths, mriflow_csv, modality='eeg')
+    eeg_zip_df_pivot = get_mri_zip_df_pivot_for_subject(eeg_zip_df,
+                                                        phoenix_paths,
+                                                        modality='eeg')
+    create_dpdash_zip_df_pivot(eeg_zip_df_pivot,
+                               dpdash_outpath,
+                               modality='eeg')
+
+
+def test_add_qc_measures():
+    data_root = Path('/data/predict1/data_from_nda')
+    phoenix_paths = [data_root / 'Pronet/PHOENIX',
+                     data_root / 'Prescient/PHOENIX']
+    mriflow_csv = data_root / 'MRI_ROOT/flow_check/mri_data_flow.csv'
+    mriqc_dir = data_root / 'MRI_ROOT/derivatives/google_qc'
+
+    mri_zip_df = get_mri_zip_df(phoenix_paths, mriflow_csv)
     mriqc_value_loc = get_most_recent_file(mriqc_dir)
-    mriqc_value_df = pd.read_csv(mriqc_value_loc, index_col=0)
 
-    mriqc_value_df.columns = ['session_id_raw', 'mriqc_val']
-    mriqc_value_df['mriqc_val'] = mriqc_value_df['mriqc_val'].str.strip()
-    mriqc_value_df['mriqc_val'] = mriqc_value_df['mriqc_val'].fillna('Pending')
-
-    ses_id_split = mriqc_value_df.session_id_raw.str.split('/')
-    mriqc_value_df['subject_id'] = ses_id_split.str[0].str.split('-').str[1]
-    mriqc_value_df['session_id'] = ses_id_split.str[1].str.split('-').str[1]
-    mriqc_value_df['date_str'] = mriqc_value_df.session_id.str[:-1]
-    mriqc_value_df['date'] = pd.to_datetime(mriqc_value_df['date_str'],
-                                            format='%Y%m%d',
-                                            errors='coerce')
-
-    # mriqc_value_df = mriqc_value_df[~mriqc_value_df['date'].isna()]
-    mriqc_value_df['scan_date_str'] = mriqc_value_df['date'].dt.strftime(
-            '%Y-%m-%d', errors='ignore')
-
-    # include timepoint
-    mriqc_value_df = pd.merge(mriqc_value_df,
-                              mri_zip_df[['subject_id', 'scan_date_str', 'timepoint']],
-                              on=['subject_id', 'scan_date_str'],
-                              how='left')
-
-
-    # remove no marked up data
-    # mriqc_value_df['mriqc_val'] = mriqc_value_df['mriqc_val'].fillna('Pending')
-    mriqc_value_df = mriqc_value_df[~mriqc_value_df['mriqc_val'].isna()]
-    # for mriqc_value_df
-    mriqc_value_df['mriqc_int'] = mriqc_value_df['mriqc_val'].str.strip().map(
-        {'Unusable': 0,
-        'Partial': 1,
-        'Pass': 2,
-        'Pending': 3}
-    )
-
+    # outpath = Path('test')
     outpath = Path('/data/predict1/data_from_nda/MRI_ROOT/eeg_mri_count')
-
-    mri_subject_dfs = pd.DataFrame()
-    for subject, table in mriqc_value_df.groupby('subject_id'):
-        site = subject[:2]
-        table = table[~table.timepoint.isna()]
-        if len(table) > 0:    
-            table= table[['subject_id', 'timepoint', 'mriqc_int']]
-            table = table.drop_duplicates()
-
-            table_pivot = pd.pivot_table(table,
-                                         index=['subject_id', 'timepoint'],
-                                         values=['mriqc_int'],
-                                         aggfunc=lambda x: str(x.iloc[0])).reset_index()
-            # table_pivot.columns
-            table_pivot.columns = ['subject_id', 'timepoint', 'mriqc_int']
-            table_pivot['day'] = 1
-            table_pivot['reftime'] = ''
-            table_pivot['timeofday'] = ''
-            table_pivot['weekday'] = ''
-
-            table_pivot = table_pivot[['day', 'reftime', 'timeofday', 'weekday', 'subject_id', 'timepoint', 'mriqc_int']]
-            mri_subject_dfs = pd.concat([mri_subject_dfs, table_pivot])
-            
-            table_pivot.to_csv(outpath / f'{site}-{subject}-mriqcval-day1to{len(table_pivot)}.csv')
+    mriqc_value_df = add_qc_measures(mri_zip_df, mriqc_value_loc)
+    mriqc_value_df_pivot = get_mriqc_value_df_pivot_for_subject(
+            mriqc_value_df, outpath)
