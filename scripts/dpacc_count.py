04cc869b45d6fb852b095afbac1d2c1b73f42581
kcho <kevincho@bwh.harvard.edu>
2023-12-26 13:09:59 -0500

    feat: merge all count related functions into dpacc_count.py

--- scripts/dpacc_count.py
+++ scripts/dpacc_count.py
@@ -1,10 +1,13 @@
-import argparse
-from argparse import RawTextHelpFormatter
 import sys
-from pathlib import Path
 import logging
+import argparse
+import pandas as pd
+from pathlib import Path
 from ampscz_asana.lib.mri_count import \
-        count_and_make_it_available_for_dpdash, merge_zip_db_and_runsheet_db
+    count_and_make_it_available_for_dpdash, \
+    merge_zip_db_and_runsheet_db
+from ampscz_asana.lib.qc import get_run_sheet_df, dataflow_dpdash
+from qqc.utils.dpdash import get_summary_included_ids
 
 
 def parse_args(argv):
@@ -16,7 +19,7 @@ def parse_args(argv):
         --phoenix_roots /data/phoenix1 /data/phoenix2 \\
         --mriflow_csv /data/mri_flow.csv \\
         --dpdash_outpath /data/dpdash_out''',
-        formatter_class=RawTextHelpFormatter)
+        formatter_class=argparse.RawTextHelpFormatter)
 
     data_root = Path('/data/predict1/data_from_nda')
     phoenix_paths = [data_root / 'Pronet/PHOENIX',
@@ -48,6 +51,10 @@ def parse_args(argv):
             default='WARNING',
             help='Log level to display (default: WARNING)')
 
+    # additional options
+    parser.add_argument('--test', action='store_true',
+            help='Test mode')
+
     # extra options
     args = parser.parse_args(argv)
     args.phoenix_roots = [Path(x) for x in args.phoenix_roots]
@@ -56,30 +63,67 @@ def parse_args(argv):
 
     return args
 
+
 if __name__ == '__main__':
     args = parse_args(sys.argv[1:])
 
+    # Configure logging
     logging.basicConfig(
             level=args.loglevel,
-            format=' %(name)s :: %(levelname)-8s :: %(message)s',
-            handlers=[logging.StreamHandler()])
+            format='%(asctime)s :: %(name)s :: %(levelname)-8s :: %(message)s',
+            handlers=[
+                    logging.StreamHandler(),  # Output to terminal
+                    logging.FileHandler('logfile.log')  # Output to log file
+            ])
+    logger = logging.getLogger(__name__)
+    logger.info('Starting the script')
+    
+    # Read all available run sheets from PHOENIX and create db (MRI)
+    logger.info('Read information from AMP-SCZ REDCap and RPMS')
+    df = pd.DataFrame()
+    for phoenix_root in args.phoenix_roots:
+        logger.info(f'Summarizing dataflow in {phoenix_root}')
+        df_tmp = get_run_sheet_df(phoenix_root,
+                                  test=args.test)
+        df = pd.concat([df, df_tmp])
+    df['entry_date'] = df['entry_date'].dt.strftime('%Y-%m-%d')
+    df.to_csv(args.mriflow_csv)
 
+    # Count data and make DPDasah readable CSV files
     for modality in 'eeg', 'mri':
+        logger.info(f'Count and make counts available for DPDash: {modality}')
         count_and_make_it_available_for_dpdash(args.phoenix_roots,
                                                args.mriflow_csv,
                                                args.dpdash_outpath,
                                                args.final_qc_dir,
-                                               modality=modality)
-
+                                               modality=modality,
+                                               sync_to_forms_id=False)
+        logger.info(f'{modality}_zip_db.csv is created')
 
     # merge zip database with the run sheet database
-    logging.info('Merging zip DB with run sheet DB')
+    logger.info('Extra processing for MRI datatype')
     zip_df_loc = args.dpdash_outpath / 'mri_zip_db.csv'
     runsheet_df_loc = args.mriflow_csv
     output_merged_zip = args.dpdash_outpath / 'mri_all_db.csv'
+    logger.info(f'Merging zip DB with run sheet DB to {output_merged_zip}')
     merge_zip_db_and_runsheet_db(zip_df_loc,
                                  runsheet_df_loc,
                                  output_merged_zip)
-    logging.info(f'{output_merged_zip} is created')
+    logger.info(f'{output_merged_zip} is created')
+    
+    logger.info(f'Creating dpdash loadable csv files')
+    df = pd.read_csv(output_merged_zip)
+    # only include subjects in forms-qc summary
+    forms_summary_ids = get_summary_included_ids()
+    df = df[df.subject.isin(forms_summary_ids)]
+    dataflow_dpdash(df, args.mriflow_csv.parent)
+    logger.info('completed')
 
-    logging.info('completed')
+    # MRIQC value match to dataflow DF
+    logger.info('Update MRIQC dpdash table according to dataflow table')
+    count_and_make_it_available_for_dpdash(args.phoenix_roots,
+                                           args.mriflow_csv,
+                                           args.dpdash_outpath,
+                                           args.final_qc_dir,
+                                           modality='mri',
+                                           sync_to_forms_id=True)
